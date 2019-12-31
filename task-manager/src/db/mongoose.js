6f6d8dd7097e2ad1faa2c7500de0ad4f32a9cf1a
teriyaki-89 <idoskhozhayev@gmail.com>
2019-12-31 11:59:50 +0600

    mongoose

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,6 +1,34 @@
 const mongoose = require("mongoose");
 
+const validator = require("validator");
+
 mongoose.connect("mongodb://127.0.0.1:27017/task-mager-api", {
     useNewUrlParser: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useUnifiedTopology: true
 });
+
+const User = mongoose.model("User", {
+    name: { type: String },
+    age: { type: Number },
+    email: {
+        type: String,
+        required: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("must be email");
+            }
+        }
+    }
+});
+
+const user = new User({ name: "Ilo", age: 30, email: "ilik@mail.kz" });
+
+user.save()
+    .then(result => {
+        //console.log(result);
+        console.log(user);
+    })
+    .catch(e => {
+        console.log(e);
+    });
