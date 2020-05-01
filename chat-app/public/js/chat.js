c56b658afd5741e5b178200b11255e6f4fdb57f6
teriyaki-89 <idoskhozhayev@beeline.kz>
2020-05-01 10:12:55 +0600

    emit events

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,2 +1,11 @@
-io();
+const socket = io();
 
+socket.on("countUpdated", (count) => {
+    console.log(`the count has been updated ${count}`);
+});
+
+
+document.querySelector('#inc').addEventListener('click',()=>{
+    //console.log('print')
+    socket.emit('increment')
+})
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -13,9 +13,19 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-io.on("connection", () => {
+let count = 0;
+
+io.on("connection", (socket) => {
     console.log("new websocket connection");
+    socket.emit("countUpdated", count);
+
+    socket.on('increment',()=>{
+        count++;
+        // socket.emit("countUpdated", count);
+        io.emit("countUpdated", count);
+    })
 });
+
 
 server.listen(port, () => {
     console.log(`app is listening on a port:  ${port}`);
